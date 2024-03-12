28e200af123cf004eab6aad6124d6eb31164be1d
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2024-03-12 16:45:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,5 @@
 sed -i "/helloworld/d" "feeds.conf.default"
 
 # 添加一个 feed 源
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
