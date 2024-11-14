c28dd33c9fec7ecc26334cf7118cc1f8c8f39d33
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2024-11-14 10:27:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,6 +18,7 @@ rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
+git clone https://github.com/QiuSimons/luci-app-daed package/dae
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
