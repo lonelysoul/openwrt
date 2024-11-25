c193d86cbdebd507b0afb0289ca0f3a04ec6f338
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2024-11-25 22:24:21 +0800

    Update init_install.sh

--- init_install.sh
+++ init_install.sh
@@ -7,14 +7,13 @@ CONFIG_URL="https://raw.githubusercontent.com/lonelysoul/openwrt/main/.config"  
 SRC_DIR="openwrt"  # 源码目录名称
 DIY_PART1_URL="https://raw.githubusercontent.com/lonelysoul/openwrt/main/diy-part1.sh"  # diy-part1 脚本 URL
 DIY_PART2_URL="https://raw.githubusercontent.com/lonelysoul/openwrt/main/diy-part2.sh"  # diy-part2 脚本 URL
-XDP_URL="https://raw.githubusercontent.com/lonelysoul/openwrt/refs/heads/main/xdp-sockets-diag.mk"
 
 # 保存当前目录
 ORIGINAL_DIR=$(pwd)
 
 # 依赖包列表
 DEPENDENCIES=(
-  ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev libreadline-dev libssl-dev libtool lrzsz mkisofs msmtp ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
+  ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev libreadline-dev libssl-dev libtool lrzsz mkisofs msmtp ninja-build p7zip p7zip-full patch pkgconf  python3 python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion swig texinfo uglifyjs  unzip vim wget xmlto xxd zlib1g-dev
 )
 
 # 更新包列表并安装依赖
